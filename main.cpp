ed867fbe11a83acb498715ca210e289698755fcd
Luis PC escritorio <luis.arroyo.90@gmail.com>
2017-04-02 20:06:25 -0300

    el premio del siglo al boludo del año 2 secuencias que buscan lo mismo

--- main.cpp
+++ main.cpp
@@ -31,7 +31,30 @@ void decreciente() {
     pila.pop();
     }
 }
+void creciente() {
+  vector<int> elementos;
+  vector<bool> validar;
+  stack<int> pila;
+  elementos.push_back(1);
+  elementos.push_back(2);
+  elementos.push_back(3);
+  elementos.push_back(4);
+  elementos.push_back(5);
+  elementos.push_back(6);
+  elementos.push_back(7);
 
+  for (int i = 0; i < 7; i++) {
+      validar.push_back(true);
+    }
+  int inicio = 0;
+  int fin = elementos.size()-1;
+  SecuCreciente(elementos,validar,pila,inicio,fin);
+  while (pila.size() != 0) {
+    cout << elementos[pila.top()]<< endl;;
+    validar[pila.top()] = true;
+    pila.pop();
+    }
+}
 void test_1() {
   vector<int> elementos;
   vector<bool> validar;
@@ -62,8 +85,15 @@ void test_2() {
   elementos.push_back(5);
   elementos.push_back(6);
   elementos.push_back(7);
+  elementos.push_back(1);
+  elementos.push_back(2);
+  elementos.push_back(3);
+  elementos.push_back(4);
+  elementos.push_back(5);
+  elementos.push_back(6);
+  elementos.push_back(7);
 
-  for (int i = 0; i < 7; i++) {
+  for (int i = 0; i < 14; i++) {
       validar.push_back(true);
     }
   int inicio = elementos.size()-1;
@@ -73,10 +103,67 @@ void test_2() {
     pila.pop();
   }
 }
+void test_3() {
+  vector<int> elementos;
+  vector<bool> validar;
+  stack<int> pila;
+  elementos.push_back(1);
+  elementos.push_back(2);
+  elementos.push_back(3);
+  elementos.push_back(4);
+  elementos.push_back(5);
+  elementos.push_back(6);
+  elementos.push_back(7);
+  elementos.push_back(1);
+  elementos.push_back(2);
+  elementos.push_back(3);
+  elementos.push_back(4);
+  elementos.push_back(5);
+  elementos.push_back(6);
+  elementos.push_back(7);
+
+  for (int i = 0; i < 14; i++) {
+      validar.push_back(true);
+    }
+  int fin = elementos.size()-1;
+  rojos(elementos,validar,pila,0,fin);
+  while (pila.size() != 0) {
+    cout << elementos[pila.top()]<< endl;
+    pila.pop();
+  }
+}
+
+void backtracking() {
+  vector<int> elementos;
+  vector<bool> validar;
+  elementos.push_back(1);
+  elementos.push_back(2);
+  elementos.push_back(3);
+  elementos.push_back(4);
+  elementos.push_back(5);
+  elementos.push_back(6);
+  elementos.push_back(7);
+  elementos.push_back(1);
+  elementos.push_back(2);
+  elementos.push_back(3);
+  elementos.push_back(4);
+  elementos.push_back(5);
+  elementos.push_back(6);
+  elementos.push_back(7);
+  for (int i = 0; i < 14; i++) {
+      validar.push_back(true);
+    }
+  int ocupados = CantidadPosicionesOcupadas(elementos,validar);
+  cout << "los ocupados son "<< ocupados << endl;
+}
+
 
 int main() {
   //decreciente();
   //test_1();
-  test_2();
+  //test_2();
+  //creciente();
+  //test_3();
+  backtracking();
   return 0;
 }
--- source.h
+++ source.h
@@ -12,6 +12,24 @@ using namespace std;
 bool diferencia(int longSecuMaxima, int longPilaActual, int posicion){
   return ( (longPilaActual + posicion) < longSecuMaxima);
   }
+
+void SecuCreciente(const vector<int> &elementos,vector<bool> &validar,stack<int> &pila,int inicio,int fin) {
+  int iterador = inicio;
+  while (iterador <= fin) {
+    if (pila.size() == 0) {
+      pila.push(iterador);
+      validar[iterador] = false;
+      }
+    else{
+      if (elementos[iterador] > elementos[pila.top()] && validar[iterador]) {
+        pila.push(iterador);
+        validar[iterador] = false;
+        }
+      }
+      iterador = iterador+1;
+    }
+  }
+
 void SecuDecreciente(const vector<int> &elementos, vector<bool> &validar,stack<int> &pila,int inicio, int fin ) {
   int iterador = inicio;
   while (iterador >= fin) {
@@ -28,17 +46,60 @@ void SecuDecreciente(const vector<int> &elementos, vector<bool> &validar,stack<i
       iterador = iterador-1;
     }
 }
-void azules(vector<int> &elementos, vector<bool> &validar,stack<int> &pilaMaxima,int inicio, int fin) {
+void rojos(const vector<int> &elementos, vector<bool> &validar,stack<int> &pilaMaxima,int inicio, int fin) {
   stack<int> pila;
   bool notermine = true;
-  pila.push(elementos.size()-1); // el primer elemento es el de mas a la derecha
   int iterador = inicio;
+  while ( (validar[iterador] == false) && (fin > iterador) ) {
+    iterador = iterador+1;
+  }
+  if (iterador <= fin) {
+    pila.push(iterador);
+  }
+  int n = inicio - fin + 1;
+  bool decero = false;
+  while (notermine && iterador <= fin) {
+    if (decero == true) {
+      pila.push(iterador);
+      decero = false;
+      validar[iterador] = false;
+    }
+    SecuCreciente(elementos,validar,pila,iterador,fin);
+    if (pilaMaxima.size()<= pila.size()) {
+      pilaMaxima = pila;
+    }
+    if ((pila.size() == n) || iterador > fin) {
+        notermine  = false;
+      }
+      else{
+      iterador = pila.top();
+      validar[pila.top()] = true;
+      pila.pop();
+      if (pila.size()==0) {
+          decero = true;
+        }
+      iterador = iterador+1;
+      }
+  }
+}
+void azules(const vector<int> &elementos, vector<bool> &validar,stack<int> &pilaMaxima,int inicio, int fin) {
+  stack<int> pila;
+  bool notermine = true;
+  int iterador = inicio;
+  while ( (validar[iterador] == false) && (fin <= iterador) ) {
+    iterador = iterador-1;
+  }
+  cout << " estoy enazules "<< endl;
+  cout << "valor de iterador "<< iterador<<endl;
+  cout <<  "valor de fin "<< fin<<endl;
+  if (fin <= iterador) {
+    pila.push(iterador);
+    cout << "entre"<< endl;
+
+  }
   int n = inicio - fin + 1;
   bool decero = false;
   while (notermine && fin <= iterador) {
-
-    int longSecuMaxima = pilaMaxima.size();
-    int longPilaActual = pila.size();
     if (decero == true) {
       pila.push(iterador);
       decero = false;
@@ -48,7 +109,7 @@ void azules(vector<int> &elementos, vector<bool> &validar,stack<int> &pilaMaxima
     if (pilaMaxima.size()<= pila.size()) {
       pilaMaxima = pila;
     }
-    if ((pila.size() == n) || diferencia(longSecuMaxima,longPilaActual,pila.top()) ) {
+    if ((pila.size() == n) || iterador < fin) {
         notermine  = false;
       }
       else{
@@ -61,4 +122,39 @@ void azules(vector<int> &elementos, vector<bool> &validar,stack<int> &pilaMaxima
       iterador = iterador-1;
       }
   }
+}
+
+void maximizar(const stack<int> &pilaRoja,const stack<int>&pilaAzul,int &ocupados) {
+  if (ocupados < pilaAzul.size()+pilaRoja.size()) {
+    ocupados = pilaAzul.size()+pilaRoja.size();
+  }
+}
+
+int CantidadPosicionesOcupadas(const vector<int> &elementos, vector<bool> &validar) {
+  int ocupados = 0;
+  int fin = elementos.size()-1;
+  for (size_t i = 0; i < elementos.size(); i++) {
+    int j = i;
+    while (j<=fin) {
+      stack<int> pilaAzul;
+      stack<int> pilaRoja;
+      rojos(elementos,validar,pilaRoja,i,fin);
+      azules(elementos,validar,pilaAzul,fin,i);
+      maximizar(pilaRoja,pilaAzul,ocupados);
+      j++;
+        cout << " roja "<< endl;
+      while (pilaRoja.size() != 0) {
+        cout << elementos[pilaRoja.top()]<< endl;;
+        validar[pilaRoja.top()] = true;
+        pilaRoja.pop();
+        }
+        cout << " az<pilaAzul "<< endl;
+        while (pilaAzul.size() != 0) {
+          cout << elementos[pilaAzul.top()]<< endl;;
+          validar[pilaAzul.top()] = true;
+          pilaAzul.pop();
+          }
+    }
+  }
+  return ocupados;
 }
